96e86e6cb01a79cbfc6d3b5c019c9910835fc5e9
Funds7 <odigwejoshua7@gmail.com>
2026-04-18 12:28:08 +0100

    Modify balance and implement buy/sell functions
    Updated balance initialization and added buy/sell functions.

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-let balance = 900;
+let balance = 1000;
 
-function sendMoney() {
+function buy() {
   balance -= 100;
+  addTrade("BUY", 100);
+  updateUI();
+}
+
+function sell() {
+  balance += 100;
+  addTrade("SELL", 100);
+  updateUI();
+}
+
+function updateUI() {
   document.getElementById("balance").innerText = balance;
+}
+
+function addTrade(type, amount) {
+  const li = document.createElement("li");
+  li.innerText = type + " $" + amount;
+  document.getElementById("trades").appendChild(li);
 }
